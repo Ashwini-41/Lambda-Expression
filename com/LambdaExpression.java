7c6db9ea3355ede68608146006c76fe866913d4d
Ashwini Garad <ashwinigarad1041@gmail.com>
2024-09-02 22:45:47 +0530

    Added number playlist and iterate

--- com/LambdaExpression.java
+++ com/LambdaExpression.java
@@ -1,26 +1,53 @@
 package com;
 
-public class LambdaExpression {
-	
-	@FunctionalInterface
-	interface MathOperation{
-		int calculate(int a, int b );
-		
-		static void result(int a, int b, String function, MathOperation obj) {
-			System.out.println("Result of " + function + " is " + obj.calculate(a, b));
-		}
-	}
-	
-	public static void main(String[] args) {
-		MathOperation addition = (a, b) -> a+b;
-		MathOperation substraction = (a,b) -> a-b;
-		MathOperation multiply = (a,b) -> a*b;
-		MathOperation division = (a,b) -> a / b;
-		
+import java.util.ArrayList;
+import java.util.function.Consumer;
+import java.util.*;
 
-		MathOperation.result(10,20,"Addition",addition);
-		MathOperation.result(30,10,"Substraction",substraction);
-		MathOperation.result(10,20,"Multiply",multiply);
-		MathOperation.result(60,20,"Division",division);
+public class LambdaExpression {
+
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>();
+		
+		for(int i=1;i<=5;i++) {
+			list.add(i);
+		}
+		
+		//method 1
+		Iterator<Integer> itr = list.iterator();
+		while(itr.hasNext()) {
+			Integer n = itr.next();
+			System.out.println("Method 1 : Iterator value:: " + n);
+		}
+		
+		//method 2 using consumer interface implementation
+		class MyConsumer implements Consumer<Integer>{
+			public void accept(Integer t) {
+				System.out.println("Method 2: Consumer impl value:: " + t);
+			}
+		}
+		
+		MyConsumer action = new MyConsumer();
+		list.forEach(action);
+		
+		//Method 3 : traversing with anonymous consumer interface implementation
+		list.forEach( new Consumer<Integer>() {
+			public void accept(Integer t) {
+				System.out.println("Method 3: For each anonymous class value :: " + t);
+			}
+		});
+		
+		//Method 4: Explicit lambda function
+		Consumer<Integer> listaction = n -> {
+			System.out.println("Method 4: For each lambda implementation value :: " + n);
+		};
+		list.forEach(listaction);
+		
+		//Method 5 :  Implicit lambda function
+		list.forEach(n -> {
+			System.out.println("Method 5: For each implementation value :: " + n);
+		});
+		
+	
 	}
 }
